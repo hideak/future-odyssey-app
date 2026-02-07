ee536786c582b9275359721e68dc3cbbad541807
Willian Hideak <whideak@gmail.com>
2026-02-07 14:58:12 +0000

    feat: Add notice in the home page for under construction website

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { TriangleAlert } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="mt-4 mx-auto">
-      Site em Construção
+    <div className="mt-12 flex flex-col items-center justify-center">
+      <TriangleAlert className="mb-4" />
+      <div>Olá!</div>
+      <div>Este site está em construção.</div>
+      <div>Volte mais tarde.</div>
     </div>
   );
 }
